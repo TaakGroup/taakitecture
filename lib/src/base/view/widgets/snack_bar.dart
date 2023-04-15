1ed7eac4b24b93abc3defcc4a0c8b11a73ecec02
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-04-15 16:36:58 +0430

    snackbar

--- lib/src/base/view/widgets/snack_bar.dart
+++ lib/src/base/view/widgets/snack_bar.dart
@@ -3,21 +3,19 @@ import 'package:get/get.dart';
 import '../../../../core/interfaces/failures.dart';
 
 SnackbarController failureSnack(Failure failure, Function() retry) {
-  return Get.showSnackbar(
-    GetSnackBar(
-      title: 'مشکلی پیش آمده',
-      message: 'ارتباط با سرور دچار مشکل شد',
-      backgroundColor: Get.theme.colorScheme.surfaceVariant,
-      snackPosition: SnackPosition.BOTTOM,
-      isDismissible: false,
-      borderRadius: 12,
-      padding: EdgeInsets.fromLTRB(24, 8, 16, 8),
-      margin: EdgeInsets.only(bottom: 72, right: 16, left: 16),
-      animationDuration: Duration(milliseconds: 500),
-      mainButton: TextButton(
-        onPressed: retry,
-        child: Text('تلاش مجدد'),
-      ),
-    ),
+  return Get.snackbar(
+    'مشکلی پیش آمده',
+    'ارتباط با سرور دچار مشکل شد',
+    backgroundColor: Get.theme.colorScheme.surfaceVariant,
+    snackPosition: SnackPosition.BOTTOM,
+    isDismissible: false,
+    borderRadius: 12,
+    padding: EdgeInsets.fromLTRB(24, 8, 16, 8),
+    margin: EdgeInsets.only(bottom: Get.height * 0.1, right: 16, left: 16),
+    animationDuration: Duration(milliseconds: 500),
+    // mainButton: TextButton(
+    //   onPressed: retry,
+    //   child: Text('تلاش مجدد'),
+    // ),
   );
 }
