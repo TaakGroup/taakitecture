ee58c335f8afefbfb7a41e11304410b1e52e729b
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-04-15 13:09:45 +0430

    failure snackbar ux/ui

--- lib/src/base/view/widgets/snack_bar.dart
+++ lib/src/base/view/widgets/snack_bar.dart
@@ -8,12 +8,12 @@ SnackbarController failureSnack(Failure failure, Function() retry) => Get.snackb
       backgroundColor: Get.theme.colorScheme.surfaceVariant,
       snackPosition: SnackPosition.BOTTOM,
       isDismissible: false,
+      borderRadius: 12,
       padding: EdgeInsets.fromLTRB(24, 8, 16, 8),
       margin: EdgeInsets.all(16),
-      duration: Duration(days: 365),
       mainButton: TextButton(
         onPressed: () {
-          Get.closeAllSnackbars();
+          Get.closeCurrentSnackbar();
           retry();
         },
         child: Text('تلاش مجدد'),
