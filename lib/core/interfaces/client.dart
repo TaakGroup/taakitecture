2e13d1732cb2dbd23ee272d17b9faf5aa51a9233
alireza <alirezakzt.cs@gmail.com>
2024-08-21 11:14:16 +0330

    Add Query Parameter to Client

--- lib/core/interfaces/client.dart
+++ lib/core/interfaces/client.dart
@@ -1,11 +1,11 @@
 abstract class IClient {
-  get(String path);
+  get(String path, Map<String, String>? query);
 
-  post(String path, {Map<String, dynamic>? data});
+  post(String path, {Map<String, dynamic>? data, Map<String, String>? query});
 
-  put(String path, {Map<String, dynamic>? data});
+  put(String path, {Map<String, dynamic>? data, Map<String, String>? query});
 
-  delete(String path, {Map<String, dynamic>? data});
+  delete(String path, {Map<String, dynamic>? data, Map<String, String>? query});
 
-  upload(String path, {required formData, Function(int, int)? onSendProgress});
+  upload(String path, {required formData, Function(int, int)? onSendProgress, Map<String, String>? query});
 }
--- lib/src/base/data/datasources/base_remote_datasource.dart
+++ lib/src/base/data/datasources/base_remote_datasource.dart
@@ -11,31 +11,31 @@ abstract class BaseRemoteDatasource<Model extends BaseModel> implements IDataSou
 
   @override
   Future<Model> find([String? params, Map<String, String>? query]) async {
-    final response = await client.get('$path/${params ?? ''}');
+    final response = await client.get('$path/${params ?? ''}', query);
     return model.fromJson(response);
   }
 
   @override
   Future<Model> create({Model? data, String? params, Map<String, String>? query}) async {
-    final response = await client.post('$path/${params ?? ''}', data: data?.toJson());
+    final response = await client.post('$path/${params ?? ''}', data: data?.toJson(), query: query);
     return model.fromJson(response);
   }
 
   @override
   Future<Model> update({required Model? data, String? params, Map<String, String>? query}) async {
-    final response = await client.put('$path/${params ?? ''}', data: data?.toJson());
+    final response = await client.put('$path/${params ?? ''}', data: data?.toJson(), query: query);
     return model.fromJson(response);
   }
 
   @override
   Future<Model> delete([String? params, Model? data, Map<String, String>? query]) async {
-    final response = await client.delete('$path/${params ?? ''}', data: data?.toJson());
+    final response = await client.delete('$path/${params ?? ''}', data: data?.toJson(), query: query);
     return model.fromJson(response);
   }
 
   @override
   uploadFile({required formData, String? params, Map<String, String>? query, Function(int, int)? onSendProgress}) async {
-    final response = await client.upload('$path/${params ?? ''}', formData: formData, onSendProgress: onSendProgress);
+    final response = await client.upload('$path/${params ?? ''}', formData: formData, onSendProgress: onSendProgress, query: query);
     return model.fromJson(response);
   }
 }
